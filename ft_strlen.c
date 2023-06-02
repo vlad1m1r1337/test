6deaa15e79ef8c7f6e2571785099ecc48fe909d0
vlad1m1r1337 <95868255+vlad1m1r1337@users.noreply.github.com>
2023-06-02 12:03:23 +0400

    len

---
+++ ft_strlen.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vgribkov <vgribkov@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/25 12:47:40 by vgribkov          #+#    #+#             */
+/*   Updated: 2023/06/02 12:02:47 by vgribkov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	if (!str)
+		return (0);
+	while (str[i])
+		i++;
+	return (i);
+}
+
+int	main(void)
+{
+	char *str = "zxc";
+	printf("%zu", ft_strlen(str));
+	return (0);
+}
